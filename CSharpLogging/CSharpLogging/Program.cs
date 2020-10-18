a816974cf9803e22983c7ad431f3faf8dced30c3
Suresh Ramakrishna <sureshramakrishna.backups@gmail.com>
2020-10-18 19:46:14 +0530

    Log4Net

---
+++ CSharpLogging/CSharpLogging/Program.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+namespace CSharpLogging
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            GlobalContext.Properties["SomeExtraDataToDisplay"] = 10;
+            log.Debug("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
